09a0fd14efe09a8c9db43409ab7a2d39f1f68cef
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-07-08 23:36:04 +0530

    now exporting parser

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import HydycoModel from "./model";
+import HydycoModel from "./parser";
 import HydycoMongoose from "./plugin";
 import MongooseExpress from "./routes";
 export { HydycoModel, HydycoMongoose, MongooseExpress };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MongooseExpress = exports.HydycoMongoose = exports.HydycoModel = void 0;
-var model_1 = require("./model");
-exports.HydycoModel = model_1.default;
+var parser_1 = require("./parser");
+exports.HydycoModel = parser_1.default;
 var plugin_1 = require("./plugin");
 exports.HydycoMongoose = plugin_1.default;
 var routes_1 = require("./routes");

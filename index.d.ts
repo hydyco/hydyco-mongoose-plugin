5661bfc7cd6dd2990e188c4f2e9e86986dc2adba
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-07-23 14:21:59 +0530

    added auto routes

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
 import HydycoModel from "./parser";
 import HydycoMongoose from "./plugin";
 import MongooseExpress from "./routes";
-export { HydycoModel, HydycoMongoose, MongooseExpress };
+declare const MongooseExpressRoutes: any;
+/**
+ * Get all mongoose express routes
+ */
+declare const HydycoRoutes: (mongooseExpressRoutes?: any) => unknown[];
+export { HydycoModel, HydycoMongoose, MongooseExpress, MongooseExpressRoutes, HydycoRoutes, };
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,30 @@ import HydycoModel from "./parser";
 import HydycoMongoose from "./plugin";
 import MongooseExpress from "./routes";
 
-export { HydycoModel, HydycoMongoose, MongooseExpress };
+import { HydycoFile } from "@hydyco/core";
+
+const files = new HydycoFile().readAllMappingFiles();
+
+const MongooseExpressRoutes: any = {};
+// create mongoose model for every json file present in project
+files.forEach((file: any) => {
+  MongooseExpressRoutes[file.name] = new MongooseExpress(file.name).Routes();
+});
+
+/**
+ * Get all mongoose express routes
+ */
+const HydycoRoutes = (mongooseExpressRoutes?: any) => {
+  if (mongooseExpressRoutes) {
+    return Object.values(mongooseExpressRoutes);
+  }
+  return Object.values(MongooseExpressRoutes);
+};
+
+export {
+  HydycoModel,
+  HydycoMongoose,
+  MongooseExpress,
+  MongooseExpressRoutes,
+  HydycoRoutes,
+};

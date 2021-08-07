372efe89569d7b6289c667747bb36c4dfd1c0fd6
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-08-07 22:43:06 +0530

    👌 IMPROVE: unwanted files removed

--- file/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Express router to handle file upload
- * @param {string} uploadDir - Directory for uploading files from root folder
- * @return {Router} router - Express router
- */
-declare const fileUpload: (uploadDir?: string) => import("express-serve-static-core").Router;
-export default fileUpload;

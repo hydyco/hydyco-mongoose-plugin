c0d45dbe792098e44f474baee3668149678d85f1
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-08-17 08:01:21 +0530

    👌 IMPROVE: removed build files

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import HydycoModel from "./parser";
-import HydycoMongoose from "./plugin";
-import MongooseExpress, { HydycoQuery } from "./routes";
-declare const MongooseExpressRoutes: any;
-/**
- * Get all mongoose express routes
- */
-declare const HydycoRoutes: (mongooseExpressRoutes?: any) => unknown[];
-export { HydycoModel, HydycoMongoose, MongooseExpress, MongooseExpressRoutes, HydycoRoutes, HydycoQuery, };
